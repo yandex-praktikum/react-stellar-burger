959b906bf985aa96f5dbf029650f09506ab9a9b0
GolotinaNina <Golotina.nina1990@gmail.com>
2023-10-11 12:08:55 +0300

    pre-Review
    CRA-заготовка и структура папок под компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,20 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import AppHeader from '../app-header/AppHeader';
+import BurgerIngredients from '../burger-ingredients/BurgerIngredients';
+import BurgerConstructor from '../burger-constructor/BurgerConstructor';
 
 function App() {
+
   return (
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
+      <AppHeader />
+      <main className={styles['app-body']}>
+        <BurgerIngredients />
+        <BurgerConstructor burgerArr={data} />
+      </main>
     </div>
+
   );
 }
 
