0687b85190494706f159076bf9bc32322c0ae3dd
GolotinaNina <Golotina.nina1990@gmail.com>
2023-10-12 11:39:38 +0300

    revert all comits

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,20 +1,16 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
-import AppHeader from '../app-header/app-header';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
 
 function App() {
-
   return (
     <div className={styles.app}>
-      <AppHeader />
-      <main className={styles['app-body']}>
-        <BurgerIngredients />
-        <BurgerConstructor burgerArr={data} />
-      </main>
+      <pre style={{
+      	margin: "auto",
+      	fontSize: "1.5rem"
+      }}>
+      	Измените src/components/app/app.jsx и сохраните для обновления.
+      </pre>
     </div>
-
   );
 }
 
