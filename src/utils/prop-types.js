bf9362fd138370482f093de5cdcfeb5d4a82f98e
GolotinaNina <Golotina.nina1990@gmail.com>
2023-10-12 11:46:46 +0300

    Revert "pre-Review"
    This reverts commit 959b906bf985aa96f5dbf029650f09506ab9a9b0.

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,17 +1,5 @@
 import PropTypes from "prop-types";
- const ingredientPropType = PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        proteins: PropTypes.number,
-        fat: PropTypes.number,
-        carbohydrates: PropTypes.number,
-        calories: PropTypes.number,
-        price: PropTypes.number.isRequired,
-        image: PropTypes.string,
-        image_mobile: PropTypes.string,
-        image_large: PropTypes.string,
-        __v: PropTypes.number,
-    });
 
-export default ingredientPropType;
+export const ingredientPropType = PropTypes.shape({
+  //укажите здесь prop-types для ингридиента
+});
