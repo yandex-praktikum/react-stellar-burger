1d6ca305b6636f49a098b76c0f92453ad8d9d56c
GolotinaNina <Golotina.nina1990@gmail.com>
2023-10-12 11:09:28 +0300

    revert
    Revert "Merge pull request #7 from GolotinaNina/revert-4-month-7/step-1"
    
    This reverts commit 161ee324a67c83e55c4cc756a44ee3175622d2c7, reversing
    changes made to e8450a6b43f743711ffb64a7c9aa4a23bdd65cf4.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,20 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
 
 function App() {
+
   return (
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
+      <AppHeader />
+      <main className={styles['app-body']}>
+        <BurgerIngredients />
+        <BurgerConstructor burgerArr={data} />
+      </main>
     </div>
+
   );
 }
 
--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,5 +1,17 @@
 import PropTypes from "prop-types";
+ const ingredientPropType = PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        proteins: PropTypes.number,
+        fat: PropTypes.number,
+        carbohydrates: PropTypes.number,
+        calories: PropTypes.number,
+        price: PropTypes.number.isRequired,
+        image: PropTypes.string,
+        image_mobile: PropTypes.string,
+        image_large: PropTypes.string,
+        __v: PropTypes.number,
+    });
 
-export const ingredientPropType = PropTypes.shape({
-  //укажите здесь prop-types для ингридиента
-});
+export default ingredientPropType;
