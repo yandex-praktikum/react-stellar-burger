7955392bb6991a47139d348fdb20cd00c20e759b
GolotinaNina <102403588+GolotinaNina@users.noreply.github.com>
2023-10-13 11:56:55 +0400

    add scroll type and fix style

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -11,31 +11,54 @@ function BurgerIngredients({ ingredients }) {
 
     const filter = (type) => ingredients.filter((el) => el.type === type);
 
+    const onTabClick = (tab) => {
+        setCurrent(tab);
+        const ref = document.getElementById(tab);
+        if (ref) {
+        ref.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    let CurrentType = '';
+
     return (
         <section className={styles.section}>
             <h1 className='text text_type_main-large pt-10 pb-5'>Соберите бургер</h1>
             <div className={`${styles['burger-menu']} pb-10`}>
-                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+                <Tab value="bun" active={current === 'bun'} onClick={() => onTabClick("bun")}>
                     Булки
                 </Tab>
-                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+                <Tab value="sauce" active={current === 'sauce'} onClick={() => onTabClick("sauce")}>
                     Соусы
                 </Tab>
-                <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+                <Tab value="main" active={current === 'main'} onClick={() => onTabClick("main")}>
                     Начинки
                 </Tab>
             </div>
-
             <div className={`${styles['burger-card']} custom-scroll`}>
-                <IngredientCards title="Булки" cardsArr={filter('bun')} />
-                <IngredientCards title="Соусы" cardsArr={filter('sauce')} />
-                <IngredientCards title="Начинки" cardsArr={filter('main')} />
+                {["bun", "sauce", "main"].map((tab) => (
+                <div key={tab} id={tab}>
+                    <h2 className="text text_type_main-medium mt-10 mb-6">
+                    {tab === "bun" ? "Булки" : tab === "sauce" ? "Соусы" : "Начинка"}
+                    </h2>
+                    <ul className={`${styles["card-list"]} pl-4 pr-4`}>
+                    {ingredients.map((item) => {
+                        if (item.type === tab && CurrentType != item.type) {
+                            CurrentType = item.type
+                            return (
+                                <IngredientCards
+                                cardsArr={filter(item.type)} />
+                            );
+                        }
+                        return null;
+                    })}
+                    </ul>
+                </div>
+                ))}
             </div>
         </section>
     );
 };
 
-BurgerIngredients.propTypes = {
-    ingredients: PropTypes.arrayOf(ingredientPropType.isRequired).isRequired,
-};
+
 export default BurgerIngredients;
