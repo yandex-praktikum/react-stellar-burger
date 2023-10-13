6867adcad9c889884b5bea9fa33ed248863e2e2f
GolotinaNina <102403588+GolotinaNina@users.noreply.github.com>
2023-10-13 09:24:42 +0400

    add api in app.jsx and comment data.js

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,14 +3,35 @@ import { data } from "../../utils/data";
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const BaseURL = 'https://norma.nomoreparties.space/api/ingredients ';
+
+  useEffect(() => {
+    fetch(BaseURL)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+      .then((data) => {
+        setData(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [BaseURL]);
+
 
   return (
     <div className={styles.app}>
       <AppHeader />
       <main className={styles['app-body']}>
-        <BurgerIngredients />
+        <BurgerIngredients ingredients = {data}/>
         <BurgerConstructor burgerArr={data} />
       </main>
     </div>
