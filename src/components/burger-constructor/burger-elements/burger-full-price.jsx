031b84c690c42ccf0b342db220ac454900e3c531
GolotinaNina <102403588+GolotinaNina@users.noreply.github.com>
2023-11-10 18:42:44 +0300

    add full-price dynamic function

---
+++ src/components/burger-constructor/burger-elements/burger-full-price.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useMemo } from "react";
+import { Button, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./burger-full-price.module.css";
+import Modal from "../../modal/Modal";
+import OrderDetails from "../../order-details/order-details";
+import { SelectedComponentContext } from "../../../services/burger-consctructor-context";
+
+
+function BurgerFullPrice() {
+    const {ingredientsConstructor} = React.useContext(SelectedComponentContext);
+    const burgerInfill = ingredientsConstructor.other;
+    const burgerBun = ingredientsConstructor.bun;
+    const [fullPriceModal, setFullPriceModal] = useState(false);
+
+
+    const handleOpenModal = () => {
+        setFullPriceModal(true);
+    }
+
+    const handleCloseModal = () => {
+        setFullPriceModal(false);
+    }
+    
+    const priceOfBurger = useMemo(() => {
+        const priceOfBun = burgerBun?.price || 0;
+        const priceOfFilling = burgerInfill.reduce((acc, item) => acc + item.price, 0);
+    
+        return priceOfBun * 2 + priceOfFilling;
+      }, [burgerInfill, burgerBun]);
+
+
+    return (
+        <div>
+            <div className={`${styles.fullPriceContainer} mt-10`}>
+                <div className={`${styles.fullPrice}`}>
+                    <span className={"text text_type_digits-medium"}>{priceOfBurger}</span>
+                    <CurrencyIcon type="primary"/>
+                </div>
+                <Button htmlType="button" type="primary" size="large" onClick={handleOpenModal}>
+                    Оформить заказ
+                </Button>
+            </div>
+            {fullPriceModal &&
+                <Modal closeModal={handleCloseModal} title={""}>
+                    <OrderDetails></OrderDetails>
+                </Modal>}
+        </div>
+    );
+}
+
+export default BurgerFullPrice;
